45a27770bc7605ee043ac654e4cf1af2eff112e1
PauPeydroFons <72466612+PauPeydroFons@users.noreply.github.com>
2020-10-13 22:52:21 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,6 +1,5 @@
 # Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
-def my_email:
-  email="Pau.Peydro@alu.uclm.es"
-  return email
 
+def my_email():
+  return "Pau.Peydro@alu.uclm.es"
